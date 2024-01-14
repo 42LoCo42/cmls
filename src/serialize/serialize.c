169008958057a7e0778baa179e6ef36634a06c58
Leon Schumacher <leonsch@protonmail.com>
2024-01-14 10:47:32 +0100

    serialize: add length encoder

--- src/serialize/serialize.c
+++ src/serialize/serialize.c
@@ -4,6 +4,29 @@
 #include <jansson.h>
 #include <stdio.h>
 
+size_t cmls_serialize_length_length(size_t len) {
+	if(len < 0x40) return 1;
+	if(len < 0x4000) return 2;
+	if(len < 0x40000000) return 4;
+	assert("INVALID LENGTH" && false);
+}
+
+void cmls_serialize_encode_header(size_t len, unsigned char* out) {
+	if(len < 0x40) {
+		out[0] = len;
+	} else if(len < 0x4000) {
+		out[0] = len >> (8 * 1) | 0x40;
+		out[1] = len >> (8 * 0) % 0x100;
+	} else if(len < 0x40000000) {
+		out[0] = len >> (8 * 3) | 0x80;
+		out[1] = len >> (8 * 2) % 0x100;
+		out[2] = len >> (8 * 1) % 0x100;
+		out[3] = len >> (8 * 0) % 0x100;
+	} else {
+		assert("INVALID LENGTH" && false);
+	}
+}
+
 bool cmls_serialize_decode_header(unsigned char* data, size_t* len) {
 	switch(data[0] >> 6) {
 	case 0:
@@ -32,5 +55,12 @@ void cmls_serialize_test(json_t* entry) {
 
 	assert(cmls_serialize_decode_header(header, &length_have));
 	assert(length_want == length_have);
+
+	unsigned char new_header[4] = {0};
+	cmls_serialize_encode_header(length_want, new_header);
+	for(size_t i = 0; i < cmls_serialize_length_length(length_want); i++) {
+		assert(header[i] == new_header[i]);
+	}
+
 	free(header);
 }
--- src/serialize/serialize.h
+++ src/serialize/serialize.h
@@ -5,7 +5,9 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-bool cmls_serialize_decode_header(unsigned char* data, size_t* len);
+size_t cmls_serialize_length_length(size_t len);
+void   cmls_serialize_encode_header(size_t len, unsigned char* out);
+bool   cmls_serialize_decode_header(unsigned char* data, size_t* len);
 
 void cmls_serialize_test(json_t* entry);
 
